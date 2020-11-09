5bbe966647824bf76c32ee8ab5faa1d2a63f0c75
Yassine JOUT <yassinejout@gmail.com>
2020-11-09 11:54:04 +0100

    fix(App): fix app title using Helmet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 import MainPage from "./pages/mainPage";
+import { Helmet } from "react-helmet";
 
+const TITLE = "Stories";
 function App() {
   return (
-    <Container>
-      <MainPage />
-    </Container>
+    <>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <Container>
+        <MainPage />
+      </Container>
+    </>
   );
 }
 
